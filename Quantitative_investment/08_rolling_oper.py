e899598b13e20a58d2659fa7be651f1e4448d83e
anyongyi <masteryongyi@163.com>
2021-04-04 03:15:12 +0800

    commit test two

--- Quantitative_investment/08_rolling_oper.py
+++ Quantitative_investment/08_rolling_oper.py
@@ -8,4 +8,5 @@ df = pd.read_csv('data/sh600000.csv', encoding='gbk', skiprows=1, nrows=5, error
 df['price'] = [1,2,3,4,5]
 
 df['收盘价_3'] = df['price'].rolling(3).mean()
+df['收盘价_3'] = df['price'].rolling(3).max()
 print(df)
